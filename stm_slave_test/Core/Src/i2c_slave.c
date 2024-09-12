7fd0b44232e8cc1298ab537ffd864d8c71365ece
spyder5658 <thesecretengineers007@gmail.com>
2024-09-12 16:06:11 +0545

    slave code updated for receiving

--- stm_slave_test/Core/Src/i2c_slave.c
+++ stm_slave_test/Core/Src/i2c_slave.c
@@ -12,13 +12,15 @@ extern I2C_HandleTypeDef hi2c1;
 
 #define RxSize 6
 uint8_t RxData[RxSize];
+int is_first_recvd = 0;
 int rxcount = 0;   //for 
 int countAddr = 0;  //debugging
 int countrxcplt = 0;   //purpose
 int counterror = 0;    //only
 
 void process_data(void){
-    
+    //memcpy(mainbuff,RxData+1,rxcount-1);
+   
 }
 
 
@@ -31,9 +33,14 @@ void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c){
 void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, uint16_t AddrMatchCode){
     if (TransferDirection == I2C_DIRECTION_TRANSMIT) //if master wants to transmit data
     {  
-        rxcount = 0;
-        countAddr++;
-        HAL_I2C_Slave_Sequential_Receive_IT(hi2c,RxData+rxcount,1 , I2C_FIRST_FRAME);
+        if(is_first_recvd == 0)
+        {
+            rxcount = 0;
+            countAddr++;
+            HAL_I2C_Slave_Sequential_Receive_IT(hi2c,RxData+rxcount,1 , I2C_FIRST_FRAME);
+
+        }
+        
     }
     else  //if master wants to receive data from slave
     {
@@ -42,25 +49,22 @@ void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, ui
 
 }
 
-void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c){
-    rxcount++;
-    if (rxcount < RxSize)
+void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+    if(is_first_recvd == 0)
     {
-        if(rxcount == RxSize-1)
-        {
-            HAL_I2C_Slave_Sequential_Receive_IT(hi2c,RxData+rxcount,1,I2C_LAST_FRAME);
-        }
-        else
-        {
-            HAL_I2C_Slave_Sequential_Receive_IT(hi2c,RxData+rxcount,1,I2C_NEXT_FRAME);
-
-        }
-
+        rxcount++;
+        is_first_recvd=1;
+        HAL_I2C_Slave_Seq_Receive_IT(hi2c,RxData+rxcount,RxData[0],I2C_LAST_FRAME);
     }
-    if (rxcount == RxSize)
+    else
     {
+        rxcount = rxcount+RxData[0];
+        is_first_recvd=0;
         process_data();
+
     }
+    
 }
 
 void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c)
